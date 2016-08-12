766be7f2313000e913e99c00cfad0902e4721692
Efremov Alexey <lexich121@gmail.com>
2016-08-12 14:30:26 +0300

    Fix

--- generators/component/templates/component_story.jsx
+++ generators/component/templates/component_story.jsx
@@ -3,6 +3,6 @@ import { storiesOf } from "@kadira/storybook";
 import <%= props.componentName %> from ".";
 
 storiesOf("<%= props.componentName %>", module)
-  .add("<%= props.componentName %> with default props", () => (
+  .add("<%= props.componentName %> with default props", ()=> (
     <<%= props.componentName %> />
   ));
--- generators/wrap/templates/component.jsx
+++ generators/wrap/templates/component.jsx
@@ -2,5 +2,5 @@ import <%= props.componentName %> from "components/<%= props.componentName %>";
 import styleCSS from "./<%= props.finalName %>.css";
 import applyStyle from "wrr-applystyle";
 
-const Wrapper = applyStyle(Hello, styleCSS);
+const Wrapper = applyStyle(<%= props.componentName %>, styleCSS);
 export default Wrapper;
